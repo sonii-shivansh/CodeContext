b8782822c086b6570018894d616fcbea92c76efb
Shivansh Soni <s.soni.shivansh@gmail.com>
2026-09-20 00:55:47 +0530

    Update server command to include host option

--- src/main/kotlin/com/codecontext/cli/ServerCommand.kt
+++ src/main/kotlin/com/codecontext/cli/ServerCommand.kt
@@ -6,15 +6,16 @@ import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.int
 import io.ktor.server.application.Application
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 class ServerCommand :
-        CliktCommand(name = "serve", help = "Start CodeContext as a SaaS API Server") {
+    CliktCommand(name = "server", help = "Start CodeContext as a SaaS API Server") {
+    private val host by option("-h", "--host", help = "Host to bind to").default("0.0.0.0")
     private val port by option("-p", "--port", help = "Port to listen on").int().default(8080)
 
     override fun run() {
-        echo("🌍 Starting CodeContext Server on port $port...")
-        embeddedServer(Netty, port = port, module = Application::module).start(wait = true)
+        echo("🌍 Starting CodeContext Server on http://$host:$port")
+        embeddedServer(Netty, host = host, port = port, module = Application::module).start(wait = true)
     }
 }
